4b02f4d40173fa2ddce4ef1a2e2862da2b9fe1d5
Naman Kukreti <namankukreti@Namans-MacBook-Pro.local>
2026-03-03 14:52:22 -0500

    Ans1 to Prob1

---
+++ Unit1 - Strings & Arrays /Problem1/ReverseSentence.py
@@ -0,0 +1,7 @@
+def reverse_sentence(sentence):
+    reversed_sentence = sentence[::-1]
+    return reversed_sentence
+print(reverse_sentence("Naman"))
+print(reverse_sentence("in reverse"))
+print(reverse_sentence("Sentence in reverse"))
+print("hello")
